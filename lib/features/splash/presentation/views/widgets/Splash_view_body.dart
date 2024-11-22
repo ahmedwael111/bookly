fa8e677792087056fdae0098b24fd279d40854c5
ahmed wael <ahmed855wael@gmail.com>
2024-11-22 02:49:36 +0200

    create Splah View body

--- lib/features/splash/presentation/views/widgets/Splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/Splash_view_body.dart
@@ -5,6 +5,8 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return const Column(
+      children: [],
+    );
   }
 }
