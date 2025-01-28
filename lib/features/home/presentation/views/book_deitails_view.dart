dc5a26d90ed6612dc4e38d20b1717325c574e0b8
ahmed wael <ahmed855wael@gmail.com>
2025-01-29 00:19:00 +0200

    create book deitals view

---
+++ lib/features/home/presentation/views/book_deitails_view.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/features/home/presentation/views/widgets/book_deitals_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDeitailsView extends StatelessWidget {
+  const BookDeitailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDeitailsViewBody(),
+    );
+  }
+}
