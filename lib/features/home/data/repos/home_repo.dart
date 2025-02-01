c7eda25a19b4344730bbd411c8577b92701d5f2c
ahmed wael <ahmed855wael@gmail.com>
2025-02-01 20:49:34 +0200

    add return type of HomeRepo and create failuer class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+// reposetry pattern
+import 'package:booklyapp/core/utils/errors.dart';
+import 'package:booklyapp/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFeatcherdBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchFeatcherdBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBooks();
 }
