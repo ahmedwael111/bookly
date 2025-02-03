835ae2ee21a4ae982fea2a3cf15ffb8c88781f9c
ahmed wael <ahmed855wael@gmail.com>
2025-02-03 03:51:00 +0200

    create Featsherd books Method

--- lib/features/home/presentation/manager/featutred_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featutred_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
-import 'package:bloc/bloc.dart';
 import 'package:booklyapp/features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchFteaturedBooks() async {
+    emit(FeaturedBooksloading());
+    var result = await homeRepo.fetchFeatcherdBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
